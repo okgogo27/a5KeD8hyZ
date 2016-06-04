f55131992ceda40b80b02db0090f14c6dcebd657
xiaodongwang <1024030474@qq.com>
2016-06-04 08:51:28 +0800

    memcachedEntity

--- src/main/java/platform/demo/entity/MemcachedEntity.java
+++ src/main/java/platform/demo/entity/MemcachedEntity.java
@@ -1,22 +1,17 @@
 package platform.demo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "memcached_entity")
 public class MemcachedEntity extends IdEntity {
 
-    private static final long serialVersionUID = 2581379023730758057L;
+	private static final long serialVersionUID = 2581379023730758057L;
 
-    private Integer memcached;
+	private Integer memcached;
 
-    public Integer getMemcached() {
-        return memcached;
-    }
+	public Integer getMemcached() {
+		return memcached;
+	}
 
-    public void setMemcached(Integer memcached) {
-        this.memcached = memcached;
-    }
+	public void setMemcached(Integer memcached) {
+		this.memcached = memcached;
+	}
 
 }
